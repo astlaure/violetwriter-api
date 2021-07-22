7656be26140d3b1541a91afee042f5aeaac6818a
Alexandre St-Laurent <alexandre.stlaurent.4@gmail.com>
2021-07-22 12:23:23 -0400

    update and delete task

--- src/tasks/task.controller.ts
+++ src/tasks/task.controller.ts
@@ -1,10 +1,22 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateTaskDTO } from './dtos/create-task.dto';
 import { Task } from './task.entity';
 import { TaskService } from './task.service';
 import { Request } from 'express';
 import { User } from '../users/user.entity';
 import { AuthenticatedGuard } from '../auth/authenticated.guard';
+import { UpdateTaskDTO } from './dtos/update-task.dto';
 
 @Controller('/tasks')
 export class TaskController {
@@ -20,5 +32,21 @@ export class TaskController {
   @UseGuards(AuthenticatedGuard)
   async postTask(@Req() req: Request, @Body() createTask: CreateTaskDTO) {
     await this.taskService.create(req.user as User, createTask);
+  }
+
+  @Patch('/:id')
+  @UseGuards(AuthenticatedGuard)
+  async patchTask(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateTask: UpdateTaskDTO,
+  ) {
+    await this.taskService.update(req.user as User, id, updateTask);
+  }
+
+  @Delete('/:id')
+  @UseGuards(AuthenticatedGuard)
+  async deleteTask(@Req() req: Request, @Param('id', ParseIntPipe) id: number) {
+    await this.taskService.delete(req.user as User, id);
   }
 }
--- src/tasks/task.service.ts
+++ src/tasks/task.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { CreateTaskDTO } from './dtos/create-task.dto';
 import { Task } from './task.entity';
 import { User } from '../users/user.entity';
+import { UpdateTaskDTO } from './dtos/update-task.dto';
 
 @Injectable()
 export class TaskService {
@@ -18,5 +19,13 @@ export class TaskService {
   async create(user: User, createTask: CreateTaskDTO) {
     const task = this.taskRepository.create({ ...createTask, user });
     await this.taskRepository.save(task);
+  }
+
+  async update(user: User, id: number, updateTask: UpdateTaskDTO) {
+    await this.taskRepository.update(id, updateTask);
+  }
+
+  async delete(user: User, id: number) {
+    await this.taskRepository.update(id, { done: true });
   }
 }
